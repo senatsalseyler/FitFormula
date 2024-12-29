c4e2f80322930b3fe9712d9c381a3615d4839934
sena <senatunc00@gmail.com>
2024-12-29 14:49:07 +0300

    fix page directions. now can save username and calorie infos.

--- FitFormula/App_Start/RouteConfig.cs
+++ FitFormula/App_Start/RouteConfig.cs
@@ -1,23 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
-using Microsoft.AspNet.FriendlyUrls;
 
-namespace FitFormula
+public class RouteConfig
 {
-    public static class RouteConfig
+    public static void RegisterRoutes(RouteCollection routes)
     {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+        routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Auth", action = "Login", id = UrlParameter.Optional }
-            );
-        }
+        routes.MapRoute(
+            name: "Default",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Auth", action = "Login", id = UrlParameter.Optional }
+        );
     }
 }
